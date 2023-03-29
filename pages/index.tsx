5c74ae8cdc93fc25e4236bf6859301a2b83d9e69
spandita dwivedi <spandita.dwi@gmail.com>
2023-03-29 21:10:42 +0530

    Final Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,13 @@ import FirstFormcomp from '../components/FirstForm/FirstForm'
 import SecondFormcomp from '../components/SecondForm/SecondForm'
 import styles from '@/styles/Home.module.scss'
 
+
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Lean-platform</title>
+      </Head>
      <FirstFormcomp/>
      
     </>
